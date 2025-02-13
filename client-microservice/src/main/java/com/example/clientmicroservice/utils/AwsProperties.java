1d37dec4771fb3df7be5217949bce7a67900da0f
AlejandroMartinez-petroprix <alejandro.martinez@petroprix.com>
2025-02-13 12:53:47 +0100

    feat: [CONFIG]: Agregar clases AwsCredentials y AwsProperties para gestión de configuración AWS

---
+++ client-microservice/src/main/java/com/example/clientmicroservice/utils/AwsProperties.java
@@ -0,0 +1,23 @@
+package com.example.clientmicroservice.utils;
+
+import com.example.clientmicroservice.utils.AwsCredentials;
+import lombok.Getter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.ConstructorBinding;
+
+@Getter
+@ConfigurationProperties(prefix = "aws")
+@ConstructorBinding
+public class AwsProperties {
+    private final String endpointOverride;
+    private final String region;
+    private final String dynamoDbTableName;
+    private final AwsCredentials credentials;
+
+    public AwsProperties(String endpointOverride, String region, String dynamoDbTableName, AwsCredentials credentials) {
+        this.endpointOverride = endpointOverride;
+        this.region = region;
+        this.dynamoDbTableName = dynamoDbTableName;
+        this.credentials = credentials;
+    }
+}
