ff50c7f4b8cf87fbb2b5691a40009130e8a1a5dc
AlejandroMartinez-petroprix <alejandro.martinez@petroprix.com>
2025-02-13 12:58:51 +0100

    feat: [MAPPER]: Agregar mapeador ClientMapper para conversión entre DTO y entidad

---
+++ client-microservice/src/main/java/com/example/clientmicroservice/mappers/ClientMapper.java
@@ -0,0 +1,18 @@
+package com.example.clientmicroservice.mappers;
+
+import com.example.clientmicroservice.model.Client;
+import com.example.clientmicroservice.model.dto.ClientInputDTO;
+import com.example.clientmicroservice.model.dto.ClientOutputDTO;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface ClientMapper {
+    ClientMapper INSTANCE = Mappers.getMapper(ClientMapper.class);
+
+    @Mapping(target = "id", ignore = true)
+    Client toEntity(ClientInputDTO dto);
+
+    ClientOutputDTO toDto(Client client);
+}
