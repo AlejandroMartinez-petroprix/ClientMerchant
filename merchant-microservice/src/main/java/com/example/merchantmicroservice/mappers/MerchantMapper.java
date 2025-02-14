629a8cf555e1f36c2e656918a800ebb9491b5e05
AlejandroMartinez-petroprix <alejandro.martinez@petroprix.com>
2025-02-14 13:50:34 +0100

    feat:[MAPPER]: Crear mapeo para Merchant

---
+++ merchant-microservice/src/main/java/com/example/merchantmicroservice/mappers/MerchantMapper.java
@@ -0,0 +1,18 @@
+package com.example.merchantmicroservice.mappers;
+
+import com.example.merchantmicroservice.model.Merchant;
+import com.example.merchantmicroservice.model.dto.MerchantInputDTO;
+import com.example.merchantmicroservice.model.dto.MerchantOutputDTO;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.springframework.stereotype.Component;
+
+@Component
+@Mapper(componentModel = "spring")
+public interface MerchantMapper {
+    @Mapping(target = "id", ignore = true)
+    Merchant toEntity(MerchantInputDTO dto);
+
+    MerchantOutputDTO toDto(Merchant merchant);
+}
+
