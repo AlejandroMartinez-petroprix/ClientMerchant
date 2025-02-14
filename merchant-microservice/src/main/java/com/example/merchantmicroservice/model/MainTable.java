130f4d922a5559ab83a3f0f61c46aee13526be7a
AlejandroMartinez-petroprix <alejandro.martinez@petroprix.com>
2025-02-14 13:42:47 +0100

    feat:[CONFIG]: Crear conexiones con dynamo y swagger para Merchant

---
+++ merchant-microservice/src/main/java/com/example/merchantmicroservice/model/MainTable.java
@@ -0,0 +1,47 @@
+package com.example.merchantmicroservice.model;
+
+import lombok.Data;
+import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.*;
+
+@Data
+@DynamoDbBean
+public class MainTable {
+    private String partitionKey;
+    private String sortKey;
+    private String id;
+    private String status;
+    private String gIndex2Pk;
+    private String createdDate;
+
+    @DynamoDbPartitionKey
+    @DynamoDbAttribute("PK")
+    public String getPartitionKey() {
+        return partitionKey;
+    }
+
+    @DynamoDbSortKey
+    @DynamoDbAttribute("SK")
+    public String getSortKey() {
+        return sortKey;
+    }
+
+    @DynamoDbAttribute("id")
+    public String getId() {
+        return id;
+    }
+
+    @DynamoDbAttribute("status")
+    public String getStatus() {
+        return status;
+    }
+
+    @DynamoDbAttribute("gIndex2Pk")
+    public String getGIndex2Pk() {
+        return gIndex2Pk;
+    }
+
+    @DynamoDbAttribute("createdDate")
+    public String getCreatedDate() {
+        return createdDate;
+    }
+}
