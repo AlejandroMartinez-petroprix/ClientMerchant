64ad5a69b84fde24296a0d89a8020dd15cdfb2a3
AlejandroMartinez-petroprix <alejandro.martinez@petroprix.com>
2025-02-18 13:27:20 +0100

    refactor:[FEIGN]: Usar dinámicamente el puerto donde se inicia merchant

--- client-microservice/src/main/java/com/example/clientmicroservice/feign/MerchantFeignClient.java
+++ client-microservice/src/main/java/com/example/clientmicroservice/feign/MerchantFeignClient.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name = "merchant-microservice", url = "http://localhost:8080")
+@FeignClient(name = "merchant-microservice", url = "${merchant.service.url}")
 public interface MerchantFeignClient {
     @GetMapping("/merchants/{id}")
     Object findById(@PathVariable("id") String merchantId, @RequestParam(required = false) boolean simpleOutput);
