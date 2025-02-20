9a63443b634ad96bf59e09bb2ffbf4426f57a84f
AlejandroMartinez-petroprix <alejandro.martinez@petroprix.com>
2025-02-20 09:21:30 +0100

    refactor:[CONTROLLER]: Añadir validaciones para los inputs

--- merchant-microservice/src/main/java/com/example/merchantmicroservice/controller/MerchantController.java
+++ merchant-microservice/src/main/java/com/example/merchantmicroservice/controller/MerchantController.java
@@ -6,6 +6,8 @@ import com.example.merchantmicroservice.service.MerchantService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
@@ -15,30 +17,30 @@ public class MerchantController {
     private final MerchantService merchantService;
 
     @PostMapping
-    public ResponseEntity<MerchantOutputDTO> createMerchant(@RequestBody MerchantInputDTO merchantInputDTO) {
+    public ResponseEntity<MerchantOutputDTO> createMerchant(@Valid @RequestBody MerchantInputDTO merchantInputDTO) {
         return ResponseEntity.ok(merchantService.createMerchant(merchantInputDTO));
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<MerchantOutputDTO> findById(@PathVariable String id, @RequestParam(required = false) boolean simpleOutput) {
+    public ResponseEntity<MerchantOutputDTO> findById(@Valid @PathVariable String id, @RequestParam(required = false) boolean simpleOutput) {
         return ResponseEntity.ok(merchantService.findById(id, simpleOutput));
     }
 
     @GetMapping("/search")
-    public ResponseEntity<List<MerchantOutputDTO>> findByName(@RequestParam String name) {
+    public ResponseEntity<List<MerchantOutputDTO>> findByName(@Valid @RequestParam String name) {
         return ResponseEntity.ok(merchantService.findByName(name));
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<MerchantOutputDTO> updateMerchant(@PathVariable String id, @RequestBody MerchantInputDTO merchantInputDTO) {
+    public ResponseEntity<MerchantOutputDTO> updateMerchant(@Valid @PathVariable String id, @RequestBody MerchantInputDTO merchantInputDTO) {
         return ResponseEntity.ok(merchantService.updateMerchant(id, merchantInputDTO));
     }
     @GetMapping("/client/{clientId}")
-    public ResponseEntity<List<MerchantOutputDTO>> findByClientId(@PathVariable String clientId) {
+    public ResponseEntity<List<MerchantOutputDTO>> findByClientId(@Valid @PathVariable String clientId) {
         return ResponseEntity.ok(merchantService.findByClientId(clientId));
     }
     @GetMapping("/{merchantId}/client")
-    public ResponseEntity<String> findClientByMerchantId(@PathVariable String merchantId) {
+    public ResponseEntity<String> findClientByMerchantId(@Valid  @PathVariable String merchantId) {
         return ResponseEntity.ok(merchantService.findClientByMerchantId(merchantId));
     }
     
