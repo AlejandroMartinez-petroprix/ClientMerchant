7e092c06c5b9f96f752e112c7df072a2150e7d78
AlejandroMartinez-petroprix <alejandro.martinez@petroprix.com>
2025-02-21 11:38:38 +0100

    Refactor:[DTO]: Eliminar anotaciones de validación del OutPutDTO

--- merchant-microservice/src/main/java/com/example/merchantmicroservice/model/dto/MerchantOutputDTO.java
+++ merchant-microservice/src/main/java/com/example/merchantmicroservice/model/dto/MerchantOutputDTO.java
@@ -12,24 +12,10 @@ import javax.validation.constraints.*;
 @NoArgsConstructor @AllArgsConstructor
 @Builder
 public class MerchantOutputDTO {
-
-    @NotNull(message = "El ID no puede ser nulo")
-    @Pattern(regexp = "^[0-9a-fA-F-]{36}$", message = "El ID debe ser un UUID válido")
     private String id;
-
-    @NotBlank(message = "El nombre del merchant no puede estar vacío")
-    @Size(min = 3, max = 50, message = "El nombre debe tener entre 3 y 50 caracteres")
     private String name;
-
-    @NotBlank(message = "La dirección no puede estar vacía")
-    @Size(min = 5, max = 100, message = "La dirección debe tener entre 5 y 100 caracteres")
     private String address;
-
-    @NotNull(message = "El tipo de merchant es obligatorio")
     private MerchantType merchantType;
-
-    @NotNull(message = "El clientId no puede ser nulo")
-    @Pattern(regexp = "^[0-9a-fA-F-]{36}$", message = "El clientId debe ser un UUID válido")
     private String clientId;
 }
 
